04680403fe4087792d9f873bbb1e81bf7d2c1651
manglemix <38180553+manglemix@users.noreply.github.com>
2020-08-08 17:39:46 +0800

    Added better velocity interpolation in classic_character

--- TrokaraScripts/classic_character.gd
+++ TrokaraScripts/classic_character.gd
@@ -12,19 +12,20 @@ func _integrate_movement(vector: Vector3, delta: float) -> Vector3:
 	if is_on_floor():
 		var new_speed := vector.length()
 		var speed := linear_velocity.length()
+		var tmp_velocity := linear_velocity
 		
 		if not is_zero_approx(new_speed):
-		# These 2 lines are to always have the linear_velocity point along the vector
-		# So that when transitioning between slopes will not affect the speed
-		# Which tends to happen because we are interpolating in between
-			linear_velocity = vector.normalized() * speed
+			# This will turn the linear_velocity such that it has the same angle of depression as the movement_vector
+			# This is so that downwaed angles are not interpolated (which would cause varying speeds when transitioning between slopes)
+			# But will still allow interpolation when the two vectors are pointing in different directions
+			tmp_velocity += vector.project(down_vector) * speed / new_speed - tmp_velocity.project(down_vector)
 		
 		# Use acceleration_weight for speeding up, and use brake_weight for slowing down
 		if new_speed >= speed:
-			return linear_velocity.linear_interpolate(vector, acceleration_weight * delta)
+			return tmp_velocity.linear_interpolate(vector, acceleration_weight * delta)
 			
 		else:
-			return linear_velocity.linear_interpolate(vector, brake_weight * delta)
+			return tmp_velocity.linear_interpolate(vector, brake_weight * delta)
 	
 	else:
 		# this will allow the linear_velocity to be modified, without changing its magnitude

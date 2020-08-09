e3c43843d0cfe1ef3766a309d5285943b880c6e9
manglemix <38180553+manglemix@users.noreply.github.com>
2020-08-09 11:42:53 +0800

    Fixed instant deceleration bug

--- TrokaraScripts/classic_character.gd
+++ TrokaraScripts/classic_character.gd
@@ -12,12 +12,12 @@ func _integrate_movement(vector: Vector3, delta: float) -> Vector3:
 	if is_on_floor():
 		var new_speed := vector.length()
 		var speed := linear_velocity.length()
-		var tmp_velocity: Vector3
+		var tmp_velocity := linear_velocity
 		
 		if (not is_zero_approx(new_speed)) and (not is_zero_approx(speed)):
 			# This will rotate the linear_velocity downward such that the angle between the up_vector and linear_velocity is equal to the angle of the up_vector to the movement_vector
 			# This is to help with preserving speeds when transitioning to a slope moving downwards
-			tmp_velocity = up_vector.rotated(up_vector.cross(linear_velocity).normalized(), up_vector.angle_to(vector)) * speed
+			tmp_velocity = up_vector.rotated(up_vector.cross(tmp_velocity).normalized(), up_vector.angle_to(vector)) * speed
 		
 		# Use acceleration_weight for speeding up, and use brake_weight for slowing down
 		if new_speed >= speed:

033f13f142f99ed9bfbb40bd359e77311facadf4
Manglemix <shabouza030@gmail.com>
2020-10-14 12:18:36 +0800

    Corrected enum references in ControllableCharacterMovement2D

--- TrokaraScripts/controllable_character_movement_2d.gd
+++ TrokaraScripts/controllable_character_movement_2d.gd
@@ -5,16 +5,16 @@ extends CharacterMovement2D
 
 func _input(event):
 	if event.is_action_pressed("sprint"):
-		movement_state = FAST
+		movement_state = MovementStates.FAST
 	
 	elif event.is_action_pressed("walk"):
-		movement_state = SLOW
+		movement_state = MovementStates.SLOW
 	
-	elif event.is_action_released("sprint") and movement_state == FAST:
-		movement_state = DEFAULT
+	elif event.is_action_released("sprint") and movement_state == MovementStates.FAST:
+		movement_state = MovementStates.DEFAULT
 	
-	elif event.is_action_released("walk") and movement_state == SLOW:
-		movement_state = DEFAULT
+	elif event.is_action_released("walk") and movement_state == MovementStates.SLOW:
+		movement_state = MovementStates.DEFAULT
 	
 	else:
 		movement_vector = Vector2(

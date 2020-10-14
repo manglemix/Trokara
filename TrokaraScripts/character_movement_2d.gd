2bb12a327cdcb8fcfc0b04004873dc1501294efc
Manglemix <shabouza030@gmail.com>
2020-10-14 12:17:53 +0800

    Refactored CharacterMovement2D

--- TrokaraScripts/character_movement_2d.gd
+++ TrokaraScripts/character_movement_2d.gd
@@ -6,16 +6,17 @@ extends Node
 
 
 enum RotationStyles {FACE_MOUSE, FACE_NODE, FACE_MOVEMENT}
-enum {DEFAULT, SLOW, FAST}
+enum MovementStates {DEFAULT, SLOW, FAST}
 
-export var sprint_speed := 10.0
+export var fast_speed := 10.0
 export var default_speed := 5.0
-export var walk_speed := 2.5
+export var slow_speed := 2.5
 export var air_speed := 5.0				# The speed when air strafing
+export(MovementStates) var movement_state := MovementStates.DEFAULT	# Corresponds to the speed that the character will move at
 export var auto_rotate := true			# If true, the body_node will be flipped to according to rotation_style
 
 # The direction the body_node will face when moving
-# FACE_MOUSE will turn towards the mouse, keep in mind that the basis_node should be the camera for this to work
+# FACE_MOUSE will turn towards the mouse
 # FACE_NODE will turn towards target_node
 # FACE_MOVMENT will turn towards the movement_vector
 export(RotationStyles) var rotation_style := RotationStyles.FACE_MOVEMENT
@@ -24,7 +25,6 @@ export var basis_node_path: NodePath = ".."							# The node which the movement 
 export var body_node_path: NodePath									# The node which will be flipped to follow the movement_vector (modifying after _ready has no effect)
 export var target_node_path: NodePath
 
-var movement_state := DEFAULT		# Corresponds to the speed that the character will move at
 var movement_vector: Vector2
 var target_node: Node2D
 
@@ -33,21 +33,22 @@ onready var basis_node: Node2D = get_node(basis_node_path)
 onready var body_node: Node2D = get_node(body_node_path)
 onready var character: Character2D = get_parent()
 
+
 func _ready():
 	if not target_node_path.is_empty():
 		target_node = get_node(target_node_path)
 
 
-func _process(delta):
+func _process(_delta):
 	var tmp_vector = basis_node.to_global(movement_vector) - basis_node.global_transform.origin
 	
 	if character.is_on_floor():
 		match movement_state:
-			FAST:
-				tmp_vector *= sprint_speed
+			MovementStates.FAST:
+				tmp_vector *= fast_speed
 			
-			SLOW:
-				tmp_vector *= walk_speed
+			MovementStates.SLOW:
+				tmp_vector *= slow_speed
 			
 			_:
 				tmp_vector *= default_speed
@@ -60,7 +61,7 @@ func _process(delta):
 	if auto_rotate:
 		match rotation_style:
 			RotationStyles.FACE_MOUSE:
-				if body_node.to_local(basis_node.to_global(get_viewport().get_mouse_position() - get_viewport().size / 2)).x < 0:
+				if body_node.to_local(body_node.get_global_mouse_position()).x < 0:
 					body_node.global_transform.x *= -1
 			
 			RotationStyles.FACE_NODE:

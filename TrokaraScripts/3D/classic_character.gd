8f8947fd5139db4287e99bb7a959ed52d76e221b
Manglemix <shabouza030@gmail.com>
2020-11-21 00:34:19 +0800

    Moved align_to_floor to _integrate_movement
    This gives more control to inherited scripts
    Classic Character has been severely simplified too

--- TrokaraScripts/3D/classic_character.gd
+++ TrokaraScripts/3D/classic_character.gd
@@ -10,24 +10,12 @@ export var brake_weight := 18.0				# weight used to interpolate velocity to 0
 
 func _integrate_movement(vector: Vector3, delta: float) -> Vector3:
 	if is_on_floor():
-		var new_speed := vector.length()
-		var speed := linear_velocity.length()
-		var tmp_velocity := linear_velocity
-		
-		if (not is_zero_approx(new_speed)) and (not is_zero_approx(speed)):
-			# This is to help with preserving speeds when transitioning to a slope moving downwards
-			tmp_velocity = movement_vector.normalized() * speed
-			
-			# To preserve the original direction
-			if linear_velocity.dot(movement_vector) < 0:
-				tmp_velocity *= -1
-		
 		# Use acceleration_weight for speeding up, and use brake_weight for slowing down
-		if new_speed >= speed:
-			return tmp_velocity.linear_interpolate(vector, acceleration_weight * delta)
+		if vector.length() >= linear_velocity.length():
+			return linear_velocity.linear_interpolate(align_to_floor(vector), acceleration_weight * delta)
 			
 		else:
-			return tmp_velocity.linear_interpolate(vector, brake_weight * delta)
+			return linear_velocity.linear_interpolate(align_to_floor(vector), brake_weight * delta)
 	
 	else:
 		# this will allow the linear_velocity to be modified, without changing its magnitude

abedb78e4045fda3ebcc0ae367940fbbe35ae1f3
Manglemix <shabouza030@gmail.com>
2020-11-20 11:36:12 +0800

    remove debug liens

--- TrokaraScripts/scrollable_kinematic_arm.gd
+++ TrokaraScripts/scrollable_kinematic_arm.gd
@@ -3,16 +3,14 @@ class_name ScrollableKinematicArm
 extends KinematicArm
 
 
-export var scroll_step := 0.5	# how much the arm moves with each scroll
+export var scroll_step := 1.0	# how much the arm moves with each scroll
 export var max_length := 10.0	# the maximum length of the arm
 
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_WHEEL_UP:
-			print(target_length)
 			target_length = clamp(current_length - scroll_step, min_length, max_length)
-			print(target_length)
 		
 		elif event.button_index == BUTTON_WHEEL_DOWN:
 			target_length = clamp(current_length + scroll_step, min_length, max_length)

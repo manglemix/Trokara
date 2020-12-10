d25a330dd980389584213bf0cc4681d69311dfd7
Manglemix <shabouza030@gmail.com>
2020-12-10 17:42:52 +0800

    Corrected jump code, including jump buffering

--- TrokaraScripts/controllable_character_jump.gd
+++ TrokaraScripts/controllable_character_jump.gd
@@ -6,10 +6,12 @@ extends CharacterJump
 # Allows the player to jump even if not on the ground, if the character lands within this amount of time
 export var jump_buffer := 0.1
 
+var _last_release_time: int		# used for jump buffering
+
 
 func _input(event):
 	if event.is_action_pressed("jump"):
-		if current_jumps > 0:
+		if not _initial_jumped or current_jumps > 0:
 			set_jumping(true)
 	
 		else:
@@ -19,12 +21,14 @@ func _input(event):
 			yield(character, "landed")
 			if (OS.get_system_time_msecs() - _last_jump_query) / 1000.0 <= jump_buffer:
 				set_jumping(true)
-				yield(self, "jumped")
 	
-				# Immediately cancel the jump if the spacebar is not pressed
+				# Cancel the jump if the spacebar is not pressed
 				# which is possible if the spacebar was released before the floor was hit
+				# factors in the time the spacebar was held down before it was released
 				if not Input.is_action_pressed("jump"):
+					yield(get_tree().create_timer((_last_release_time - _last_jump_query) / 1000.0), "timeout")
 					set_jumping(false)
 	
 	elif event.is_action_released("jump"):
+		_last_release_time = OS.get_system_time_msecs()
 		set_jumping(false)

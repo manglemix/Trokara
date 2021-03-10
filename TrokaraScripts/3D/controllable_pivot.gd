31698fbd063092aed6cd6192776c818339625cf9
Manglemix <shabouza030@gmail.com>
2021-03-10 11:14:50 +0800

    Added joypad threshold

--- TrokaraScripts/3D/controllable_pivot.gd
+++ TrokaraScripts/3D/controllable_pivot.gd
@@ -5,19 +5,35 @@ extends PivotPoint
 
 
 export var mouse_sensitivity := 0.002
-export var max_speed := 1.0				# used to determine how fast the joypad can rotate this pivot
 export var invert_x := false
 export var invert_y := false
-export var accept_input := true setget set_accept_input		# If true, this node can accept user input for rotating
+export var mouse_lock := true setget set_mouse_lock		# If true, this node can accept user input for rotating
+export var max_speed := 1.0				# used to determine how fast the joypad can rotate this pivot
+export var joypad_threshold := 0.05
+
+var joypad_idx: int
 
 
 func _ready():
 	# has to be called again as the setter for the export var is called before ready (which doesnt change process input)
-	set_accept_input(accept_input)
+	set_mouse_lock(mouse_lock)
+	update_joypad()
+	Input.connect("joy_connection_changed", self, "update_joypad")
 
 
-func set_accept_input(value: bool) -> void:
-	accept_input = value
+func update_joypad(_device=0, _connected=true) -> void:
+	var connected_joypads := Input.get_connected_joypads()
+	
+	if not connected_joypads.empty():
+		set_process(true)
+		joypad_idx = connected_joypads[0]
+	
+	else:
+		set_process(false)
+
+
+func set_mouse_lock(value: bool) -> void:
+	mouse_lock = value
 	set_process_input(value)
 	
 	if value:
@@ -40,16 +56,19 @@ func _input(event):
 
 
 func _process(delta: float):
-	var connected_joypads := Input.get_connected_joypads()
+	var axis_vectors := Vector2(Input.get_joy_axis(joypad_idx, JOY_ANALOG_RX), Input.get_joy_axis(joypad_idx, JOY_ANALOG_RY))
 	
-	if not connected_joypads.empty():
-		var device: int = connected_joypads[0]
-		var axis_vectors := Vector2(Input.get_joy_axis(device, JOY_ANALOG_RX), Input.get_joy_axis(device, JOY_ANALOG_RY)) * max_speed * delta
-		
-		if invert_x:
-			axis_vectors.x *= -1
-		
-		if invert_y:
-			axis_vectors.y *= -1
-		
-		biaxial_rotate(axis_vectors.y, axis_vectors.x)
+	if abs(axis_vectors.x) < joypad_threshold:
+		axis_vectors.x = 0
+	
+	elif invert_x:
+		axis_vectors.x *= -1
+	
+	if abs(axis_vectors.y) < joypad_threshold:
+		axis_vectors.y = 0
+	
+	elif invert_y:
+		axis_vectors.y *= -1
+	
+	axis_vectors *= max_speed * delta
+	biaxial_rotate(axis_vectors.y, axis_vectors.x)
